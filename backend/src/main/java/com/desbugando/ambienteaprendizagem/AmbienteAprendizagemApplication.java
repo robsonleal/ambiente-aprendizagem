e334e9044de6560b882c3c71d85e36dca193f3e7
Robson Leal <robson_leal@outlook.com.br>
2023-02-15 10:27:09 -0300

    :building_construction: Criando o projeto backend

---
+++ backend/src/main/java/com/desbugando/ambienteaprendizagem/AmbienteAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package com.desbugando.ambienteaprendizagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbienteAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbienteAprendizagemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desbugando/ambienteaprendizagem/AmbienteAprendizagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desbugando.ambienteaprendizagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbienteAprendizagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
